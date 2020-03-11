9bd37bd943efea2a5d5da66e57662502b8d1d635
AlexSE-Git <AlexSE2020@outlook.com>
2020-03-11 17:06:30 -0400

    March 11th: Removed prints for previous testing

--- src/main/java/edu/duke/ece651/server/Executor.java
+++ src/main/java/edu/duke/ece651/server/Executor.java
@@ -13,8 +13,8 @@ public class Executor {
         int rand_index = rand.nextInt(keyArray.length);
         int squad_id = keyArray[rand_index];
         int soldier_num = attackSquads.remove(squad_id); // Remove from attackSquads
-        System.out.println("Selecting attacker ... Squad " + squad_id 
-                                               + "! (" + soldier_num + " soldiers)");
+        // System.out.println("Selecting attacker ... Squad " + squad_id 
+        //                                        + "! (" + soldier_num + " soldiers)");
     
         ForcePair out = new ForcePair(squad_id, soldier_num);
 
@@ -33,36 +33,36 @@ public class Executor {
         while (attacker.getSoldier() != 0 && defender.getSoldier() != 0) {
             int attDice = rand.nextInt();
             int defDice = rand.nextInt();
-            System.out.println("Attacker (owned by " + attacker.getOwner() + 
-                                   ") current: " + attacker.getSoldier() + " soldiers");
-            System.out.println("Defender (owned by " + defender.getOwner() + 
-                                   ") current: " + defender.getSoldier() + " soldiers");
-            System.out.println("Attacker roll dice: " + attDice);
-            System.out.println("Defender roll dice: " + defDice);
+            // System.out.println("Attacker (owned by " + attacker.getOwner() + 
+            //                        ") current: " + attacker.getSoldier() + " soldiers");
+            // System.out.println("Defender (owned by " + defender.getOwner() + 
+            //                        ") current: " + defender.getSoldier() + " soldiers");
+            // System.out.println("Attacker roll dice: " + attDice);
+            // System.out.println("Defender roll dice: " + defDice);
             if (attDice <= defDice) {
                 attacker.lossFight();
-                System.out.println("Attacker lost a fight!");
+                // System.out.println("Attacker lost a fight!");
             } 
             else {
                 defender.lossFight();
-                System.out.println("Defender lost a fight!");
+                // System.out.println("Defender lost a fight!");
             }
         }
 
         if (attacker.getSoldier() == 0) {
-            System.out.println("Defender won the combat!");
+            // System.out.println("Defender won the combat!");
             return defender;
         } 
         else {
-            System.out.println("Attacker won the combat!");
+            // System.out.println("Attacker won the combat!");
             return attacker;
         }
   }
 
     private void assertDomination(ForcePair survivor, Territory terr) {
      
-        System.out.println(terr.getName() + "'s new owner is " + survivor.getOwner() + 
-                                   " with " + survivor.getSoldier() + " soldiers");
+        // System.out.println(terr.getName() + "'s new owner is " + survivor.getOwner() + 
+        //                            " with " + survivor.getSoldier() + " soldiers");
         terr.setOwner(survivor.getOwner());
         terr.setNum(survivor.getSoldier());
     }
@@ -72,11 +72,11 @@ public class Executor {
 
         // Loop through each land
         for (String currBFD : battleFields) { // For each current battle field
-            System.out.println("Current battle field is: " + currBFD + "!");
+            // System.out.println("Current battle field is: " + currBFD + "!");
             Map<Integer, Integer> attackSquads = attacks.get(currBFD);
             Territory currTerr = territories.get(currBFD);
-            System.out.println(currBFD + " is owned by: " + currTerr.getOwner() 
-                                        + " with " + currTerr.getNum() + " soldiers.");
+            // System.out.println(currBFD + " is owned by: " + currTerr.getOwner() 
+            //                             + " with " + currTerr.getNum() + " soldiers.");
             ForcePair attacker = new ForcePair();
             ForcePair defender = new ForcePair();
 
