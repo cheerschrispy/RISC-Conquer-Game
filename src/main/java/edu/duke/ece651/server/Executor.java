c442adfa826577d52f95b85cc257a9b5abf39ee3
AlexSE-Git <AlexSE2020@outlook.com>
2020-03-11 16:53:12 -0400

    March 11th: Implemented attack resolution

--- src/main/java/edu/duke/ece651/server/Executor.java
+++ src/main/java/edu/duke/ece651/server/Executor.java
@@ -3,11 +3,94 @@ package edu.duke.ece651.server;
 import java.util.*;
 
 public class Executor {
+    
+    private ForcePair selectAttacker(Map<Integer, Integer> attackSquads) {
+    
+        Integer[] keyArray = attackSquads.keySet().toArray(new Integer[0]); // Store all keys in an array
+        Random rand = new Random(System.currentTimeMillis());
 
+        // Generate random integers in range 0 to keyArray size
+        int rand_index = rand.nextInt(keyArray.length);
+        int squad_id = keyArray[rand_index];
+        int soldier_num = attackSquads.remove(squad_id); // Remove from attackSquads
+        System.out.println("Selecting attacker ... Squad " + squad_id 
+                                               + "! (" + soldier_num + " soldiers)");
+    
+        ForcePair out = new ForcePair(squad_id, soldier_num);
+
+        return out;
+    }
+
+    private ForcePair deployDefender(Territory terr) {
+  
+        ForcePair out = new ForcePair(terr.getOwner(), terr.getNum());
+        return out;
+    }
+
+    private ForcePair combat(ForcePair attacker, ForcePair defender) {
+    
+        Random rand = new Random(System.currentTimeMillis());
+        while (attacker.getSoldier() != 0 && defender.getSoldier() != 0) {
+            int attDice = rand.nextInt();
+            int defDice = rand.nextInt();
+            System.out.println("Attacker (owned by " + attacker.getOwner() + 
+                                   ") current: " + attacker.getSoldier() + " soldiers");
+            System.out.println("Defender (owned by " + defender.getOwner() + 
+                                   ") current: " + defender.getSoldier() + " soldiers");
+            System.out.println("Attacker roll dice: " + attDice);
+            System.out.println("Defender roll dice: " + defDice);
+            if (attDice <= defDice) {
+                attacker.lossFight();
+                System.out.println("Attacker lost a fight!");
+            } 
+            else {
+                defender.lossFight();
+                System.out.println("Defender lost a fight!");
+            }
+        }
+
+        if (attacker.getSoldier() == 0) {
+            System.out.println("Defender won the combat!");
+            return defender;
+        } 
+        else {
+            System.out.println("Attacker won the combat!");
+            return attacker;
+        }
+  }
+
+    private void assertDomination(ForcePair survivor, Territory terr) {
+     
+        System.out.println(terr.getName() + "'s new owner is " + survivor.getOwner() + 
+                                   " with " + survivor.getSoldier() + " soldiers");
+        terr.setOwner(survivor.getOwner());
+        terr.setNum(survivor.getSoldier());
+    }
 
     public void attack(Map<String, Map<Integer, Integer>> attacks, Map<String, Territory> territories) {
+        Set<String> battleFields = attacks.keySet(); // Get the names of all lands
+
+        // Loop through each land
+        for (String currBFD : battleFields) { // For each current battle field
+            System.out.println("Current battle field is: " + currBFD + "!");
+            Map<Integer, Integer> attackSquads = attacks.get(currBFD);
+            Territory currTerr = territories.get(currBFD);
+            System.out.println(currBFD + " is owned by: " + currTerr.getOwner() 
+                                        + " with " + currTerr.getNum() + " soldiers.");
+            ForcePair attacker = new ForcePair();
+            ForcePair defender = new ForcePair();
+
+            defender = deployDefender(currTerr);
+            // Loop until one survivor
+            while (!attackSquads.isEmpty()) {
+                attacker = selectAttacker(attackSquads); // Select and remove a attacker from attackSquads
+                defender = combat(attacker, defender); // One that survive become new defender
+            }
+            assertDomination(defender, currTerr); // Write combat result in currTerr
+        }
 
     }
+
 
     public void execute(List<Player> players, Map<String, Territory> territories) {
         for (Player player : players) {
