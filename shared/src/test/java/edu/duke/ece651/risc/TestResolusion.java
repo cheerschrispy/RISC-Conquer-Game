32fb770aad37da08c9b588cdbc7f760f2ac8eaf7
zp38 <zp38@duke.edu>
2020-04-06 15:07:10 -0400

    April 6th: Reconfigured Junit test for combat resolusion

--- shared/src/test/java/edu/duke/ece651/risc/TestResolusion.java
+++ shared/src/test/java/edu/duke/ece651/risc/TestResolusion.java
@@ -6,42 +6,85 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class TestResolusion {
-//   @Test
-//   public void test_attack() {
-//   	Executor tEX = new Executor();
-//   	Map<String, Territory> territories = new HashMap<String, Territory>();
-//   	Map<String, Map<Integer, Integer>> testAttacks = new HashMap<String, Map<Integer, Integer>>();
+    @Test
+    public void test_attack() {
+        Executor tEX = new Executor();
+        Map<String, Territory> territories = new HashMap<String, Territory>();
+        Map<String, Map<Integer, ArrayList<Unit>>> testAttacks = new HashMap<String, Map<Integer, ArrayList<Unit>>>();
 
-//     Territory terrO = new Territory("OZ", 1, 25);
-//     Territory terrA = new Territory("Azeroth", 2, 20);
-//     Territory terrL = new Territory("Lordaeron", 3, 20);
-// 	  territories.put("OZ", terrO);
-// 	  territories.put("Azeroth", terrA);
-// 	  territories.put("Lordaeron", terrL);
 
-//   	Map<Integer, Integer> testOZ = new HashMap<Integer, Integer>();
-//   	testOZ.put(1, 5);
-//   	testOZ.put(2, 7);
-//   	testOZ.put(3, 10);
-//   	Map<Integer, Integer> testAzeroth = new HashMap<Integer, Integer>();
-//   	testAzeroth.put(1, 8);
-//   	testAzeroth.put(2, 5);
-//   	testAzeroth.put(3, 6);
-//   	Map<Integer, Integer> testLordaeron = new HashMap<Integer, Integer>();
-//   	testLordaeron.put(1, 3);
-//   	testLordaeron.put(2, 7);
-//   	testLordaeron.put(3, 7);
 
-//   	testAttacks.put("OZ", testOZ);
-//   	testAttacks.put("Azeroth", testAzeroth);
-//   	testAttacks.put("Lordaeron", testLordaeron);
+        ArrayList<Unit> ozListA = new ArrayList<Unit>(
+                Arrays.asList(new Unit(4),
+                        new Unit(11),
+                        new Unit(20),
+                        new Unit(4))
+        );
 
-//   	tEX.attack(testAttacks, territories);
-//     Set<String> tkeys = territories.keySet();
-// for(String k: tkeys) {
-//     System.out.println(k + " is now owned by " + territories.get(k).getOwner()
-//                                     + " with " + territories.get(k).getNum() + " soldiers");
-//   }
-//   }
+        ArrayList<Unit> azerothListA = new ArrayList<Unit>(
+                Arrays.asList(new Unit(14),
+                        new Unit(10),
+                        new Unit(6),
+                        new Unit(9))
+        );
+
+        ArrayList<Unit> lordaeronListA = new ArrayList<Unit>(
+                Arrays.asList(new Unit(3),
+                        new Unit(22),
+                        new Unit(2),
+                        new Unit(15))
+        );
+        ArrayList<Unit> ozListD = new ArrayList<Unit>(
+                Arrays.asList(new Unit(25),
+                        new Unit(11),
+                        new Unit(20),
+                        new Unit(14))
+        );
+
+        ArrayList<Unit> azerothListD = new ArrayList<Unit>(
+                Arrays.asList(new Unit(5),
+                        new Unit(3),
+                        new Unit(3),
+                        new Unit(9))
+        );
+
+        ArrayList<Unit> lordaeronListD = new ArrayList<Unit>(
+                Arrays.asList(new Unit(3),
+                        new Unit(22),
+                        new Unit(2),
+                        new Unit(15))
+        );
+
+        Territory terrO = new Territory("OZ", 1);
+        terrO.setSoldiers(ozListD);
+        Territory terrA = new Territory("Azeroth", 2);
+        terrA.setSoldiers(azerothListD);
+        Territory terrL = new Territory("Lordaeron", 3);
+        terrL.setSoldiers(lordaeronListD);
+
+        territories.put("OZ", terrO);
+        territories.put("Azeroth", terrA);
+        territories.put("Lordaeron", terrL);
+
+        Map<Integer, ArrayList<Unit>> testOZ = new HashMap<Integer, ArrayList<Unit>>();
+        testOZ.put(2, ozListA);
+
+        Map<Integer, ArrayList<Unit>> testAzeroth = new HashMap<Integer, ArrayList<Unit>>();
+        testAzeroth.put(3, azerothListA);
+
+        Map<Integer, ArrayList<Unit>> testLordaeron = new HashMap<Integer, ArrayList<Unit>>();
+        testLordaeron.put(1, lordaeronListA);
+
+
+        testAttacks.put("OZ", testOZ);
+        testAttacks.put("Azeroth", testAzeroth);
+        testAttacks.put("Lordaeron", testLordaeron);
+
+        tEX.attack(testAttacks, territories);
+        Set<String> tkeys = territories.keySet();
+        for(String k: tkeys) {
+            System.out.println(k + " is now owned by " + territories.get(k).getOwner());
+        }
+    }
 
 }
