d4c3cae03e1a02424de6a6e74271a785079cb5df
zp38 <zp38@duke.edu>
2020-04-06 21:06:42 -0400

    April 6th: Reconfigured Junit test for Action

--- shared/src/test/java/edu/duke/ece651/risc/TestAction.java
+++ shared/src/test/java/edu/duke/ece651/risc/TestAction.java
@@ -4,14 +4,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 public class TestAction {
   @Test
   public void test_() {
-    Action action=new Action("M","a1","b1",null);
-    assertEquals("M",action.getName());
-    assertEquals("a1", action.getStart());
-    assertEquals("b1", action.getEnd());
-//    assertEquals(2, action.getNum());
+    Action actionM = new Action("M","a1","b1", new ArrayList<Unit>());
+    Action actionU = new Action("U","a2", new ArrayList<Unit>(), 3);
+    Action actionT = new Action("T");
+
+    assertEquals("M", actionM.getName());
+    assertEquals("a1", actionM.getStart());
+    assertEquals("b1", actionM.getEnd());
+
+    actionM.addSoldier(new Unit());
+    assertEquals(3, actionU.getLevels());
+    actionU.getSoldiers();
+
     
   }
 
