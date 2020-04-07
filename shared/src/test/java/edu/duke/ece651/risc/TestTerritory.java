738aa173f951ff031ca6d51329a78e2a41a4591c
zp38 <zp38@duke.edu>
2020-04-06 21:09:04 -0400

    April 6th: Reconfigured Junit test for Territory

--- shared/src/test/java/edu/duke/ece651/risc/TestTerritory.java
+++ shared/src/test/java/edu/duke/ece651/risc/TestTerritory.java
@@ -7,17 +7,24 @@ import org.junit.jupiter.api.Test;
 public class TestTerritory {
   @Test
   public void test_() {
-    Territory china = new Territory("China",0,100);
-    Territory japan = new Territory("Japan",1,50);
+
+
+    Territory china = new Territory("China",0);
+    china.initSoldiers(100);
+    Territory japan = new Territory("Japan",1);
+    japan.initSoldiers(50);
     china.connect(japan);
+
     assertEquals(true, china.getNeighbors().contains(japan));
     assertEquals("China", china.getName());
     assertEquals(0, china.getOwner());
-    assertEquals(100, china.getNum());
-    china.setNum(101);
-    assertEquals(101, china.getNum());
+
     china.setOwner(2);
     assertEquals(2, china.getOwner());
+    assertEquals(3, china.getSize());
+    china.setSoldiers(japan.getSoldiers());
+
+    assertEquals(100, china.getSoldierOfLevel(0, china.getSoldierNumOfLevel(0)));
     
   }
 
