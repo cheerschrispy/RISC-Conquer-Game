a108a5d5f1f840e712b201ce553a3f6ddba84080
zp38 <zp38@duke.edu>
2020-04-06 21:08:30 -0400

    April 6th: Reconfigured Junit test for Player

--- shared/src/test/java/edu/duke/ece651/risc/TestPlayer.java
+++ shared/src/test/java/edu/duke/ece651/risc/TestPlayer.java
@@ -8,6 +8,8 @@ import java.util.*;
 
 import org.junit.jupiter.api.Test;
 
+import javax.swing.*;
+
 public class TestPlayer {
   // private Territory territories;
   public  Map<String, Territory> territories=new HashMap<>();
@@ -23,13 +25,13 @@ public class TestPlayer {
     Scanner sc = new Scanner(System.in);
     
     HashMap<String, Integer> init_info = new HashMap<>();
-    player.initial_game(territories, sc, 9, init_info);
+    player.initial_game(territories, sc, 9, init_info, new JTextArea());
     player.addAction(territories, "a", sc);
     territories.get("a2").setOwner(1);
     player.addAction(territories, "a", sc);
 
     
-    player.addAction_afterFail(territories);
+    player.addAction_afterFail(territories, new JTextArea());
     //player.addAction(territories, "a", sc);
     
   }
@@ -41,7 +43,7 @@ public class TestPlayer {
         for (int i = 0; i < player_num; i++) {
             for (int j = 0; j < 3; j++) {
                 String tName = (char) ('a' + i) + String.valueOf(j);
-                Territory newTerritory = new Territory(tName, i, 3);
+                Territory newTerritory = new Territory(tName,  3);
                 territories.put(tName, newTerritory);
                 matrix[i][j] = newTerritory;
                 if (j > 0) {
